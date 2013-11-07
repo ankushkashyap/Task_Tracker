9cb8773917e67e50ee24f9c52f6a61c778b4ba93
pardeepcheema <pardeepcheema2@gmail.com>
2013-11-06 20:18:56 -0500

    Login

---
+++ Login/AuthServlet.java
@@ -0,0 +1,100 @@
+
+package servlets.common;
+
+import beans.common.LoginBean;
+import datalayer.common.CommonServices;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author student
+ */
+@WebServlet(name = "AuthServlet", urlPatterns = {"/AuthServlet"})
+public class AuthServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String username = request.getParameter("txtUserName");
+            String password = request.getParameter("txtPassword");
+            CommonServices objDB=new CommonServices();
+            LoginBean objBean = objDB.authenticateUser(username, password);
+            
+            if (objBean == null) {
+            response.sendRedirect("Login_mp.jsp?msg=Invalid Username/Password");
+            } else {
+                if (objBean.getStatus()==0) {
+                    response.sendRedirect("Login_mp.jsp?msg=Access Denied By Administrator");
+                } else {
+                    HttpSession session = request.getSession(true);
+                    session.setAttribute("User_Name", username);
+                    session.setAttribute("User_Id", objBean.getUser_Id());
+                    session.setAttribute("User_Type", objBean.getUser_Type());
+                    response.sendRedirect("module/Home.jsp");
+                }
+            }
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
