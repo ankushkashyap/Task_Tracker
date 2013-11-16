5d942ac1c4befa63eb1013fe2cc14d7830d4f443
dagz213 <pizza_aldwyn@yahoo.com>
2013-11-16 15:02:07 -0500

    Manage Project Type

---
+++ Manage Project Type/ManageStatusBean.java
@@ -0,0 +1,39 @@
+package beans.module;
+
+public class ManageStatusBean {
+
+    private int status_Id, is_Active;
+    private String title, description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getIs_Active() {
+        return is_Active;
+    }
+
+    public void setIs_Active(int is_Active) {
+        this.is_Active = is_Active;
+    }
+
+    public int getStatus_Id() {
+        return status_Id;
+    }
+
+    public void setStatus_Id(int status_Id) {
+        this.status_Id = status_Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
